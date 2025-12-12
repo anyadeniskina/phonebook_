b90e6e4f5c4a5741a679ab0804296cb4ed127048
Anna <deniskina02@mail.ru>
2025-12-12 12:15:39 +0300

    Initial empty main branch

--- FileManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "PhoneBook.h"
-#include <string>
-
-class FileManager {
-public:
-    static bool backupData(const PhoneBook& phoneBook, const std::string& filename);
-    static bool restoreFromBackup(PhoneBook& phoneBook, const std::string& filename);
-};
